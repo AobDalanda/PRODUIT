f12058bfdfeadeb604173a9c5cb9758dfae38842
bsidy <bsidy01@gmail.com>
2021-08-26 06:28:07 +0200

    add-produit component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProduitsComponent} from "./produits/produits.component";
+import {AddProduitComponent} from "./add-produit/add-produit.component";
 
 const routes: Routes = [
-
+  {path:"produits" , component:ProduitsComponent},
+  {path:"addproduit", component:AddProduitComponent},
+  {path:"", redirectTo:"produits",pathMatch: "full"}
 ];
 
 @NgModule({
